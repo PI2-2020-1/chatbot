6055976d0f624678ab8b9e71425a9f396c2518a4
GeovanaRamos <geovannna.1998@gmail.com>
2020-10-16 14:57:14 -0300

    Adding temperature verification through api

--- actions.py
+++ actions.py
@@ -1,28 +1,45 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
-
-
-# This is a simple example for a custom action which utters "Hello World!"
+import requests
+import logging
 
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-import random
+import random, time
+
+logger = logging.getLogger(__name__)
 
 class ActionTemperatura(Action):
 
     def name(self) -> Text:
         return "action_temperatura"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    async def run(
+        self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        
+        events = tracker.current_state()['events']
+        user_events = []
+        for e in events:
+            if e['event'] == 'user':
+                user_events.append(e)
 
+        metadata = user_events[-1]['metadata']
+
+        #verifica usuario telegram
+        r = requests.get('http://localhost:8000/api/telegram/verification/' + metadata)
+
+        if r.status_code == 404:
+            dispatcher.utter_message(text="Usuário não encontrado no sistema A2P2.")
+            return []
+
+        response = r.json()
+
+        dispatcher.utter_message(text="Pegando a temperatura para a fazenda de " + response['full_name'] + "...")
+        time.sleep(2)
+
+        #verifica temperatura
         value = random.randint(20, 45)
         text = "A temperatura atual do solo é de {} graus.".format(value)
 
@@ -35,12 +52,12 @@ class ActionUmidade(Action):
     def name(self) -> Text:
         return "action_umidade"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    async def run(
+        self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
         value = random.randint(0, 100)
-        text = "A umidade atual do solo é de {}%.".format(value)
+        text = "A umidade atual do olo é de {}%.".format(value)
 
         dispatcher.utter_message(text=text)
 
@@ -51,9 +68,9 @@ class ActionPH(Action):
     def name(self) -> Text:
         return "action_ph"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    async def run(
+        self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
         value = random.randint(1, 10)
         text = "O ph atual do solo é de {}.".format(value)
---
+++ connector/connector.py
@@ -0,0 +1,20 @@
+import logging
+from sanic.request import Request
+from telegram import (
+    Update,
+)
+from typing import Dict, Text, Any, Optional
+
+from rasa.core.channels.telegram import TelegramInput
+
+logger = logging.getLogger(__name__)
+
+
+class CustomTelegramInput(TelegramInput):
+
+    def get_metadata(self, request: Request) -> Optional[Dict[Text, Any]]:
+        update = Update.de_json(request.json, self.get_output_channel())
+        username = update.message.chat.username 
+        return username
+
+   
