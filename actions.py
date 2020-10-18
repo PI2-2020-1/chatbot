810fb6181746552903e80e9427af51b2fac6438d
GeovanaRamos <geovannna.1998@gmail.com>
2020-10-17 21:07:44 -0300

    Adding telegram verification to all actions

--- actions.py
+++ actions.py
@@ -6,9 +6,34 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-import random, time
+import random
+import time
 
 logger = logging.getLogger(__name__)
+
+
+def get_telegram_username(tracker):
+    events = tracker.current_state()['events']
+    user_events = []
+    
+    for e in events:
+        if e['event'] == 'user':
+            user_events.append(e)
+
+    return user_events[-1]['metadata']
+
+
+def verify_telegram(dispatcher, tracker):
+    r = requests.get(
+        'http://localhost:8000/api/telegram/verification/' + get_telegram_username(tracker))
+
+    if r.status_code == 404:
+        dispatcher.utter_message(
+            text="Usuário não encontrado no sistema A2P2.")
+        return None
+
+    return r.json()
+
 
 class ActionTemperatura(Action):
 
@@ -18,34 +43,23 @@ class ActionTemperatura(Action):
     async def run(
         self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        
-        events = tracker.current_state()['events']
-        user_events = []
-        for e in events:
-            if e['event'] == 'user':
-                user_events.append(e)
 
-        metadata = user_events[-1]['metadata']
+        response = verify_telegram(dispatcher, tracker)
 
-        #verifica usuario telegram
-        r = requests.get('http://localhost:8000/api/telegram/verification/' + metadata)
-
-        if r.status_code == 404:
-            dispatcher.utter_message(text="Usuário não encontrado no sistema A2P2.")
+        if not response:
             return []
 
-        response = r.json()
-
-        dispatcher.utter_message(text="Pegando a temperatura para a fazenda de " + response['full_name'] + "...")
+        dispatcher.utter_message(
+            text="Pegando a temperatura para a fazenda de " + response['full_name'] + " ...")
         time.sleep(2)
 
-        #verifica temperatura
         value = random.randint(20, 45)
         text = "A temperatura atual do solo é de {} graus.".format(value)
 
         dispatcher.utter_message(text=text)
 
         return []
+
 
 class ActionUmidade(Action):
 
@@ -56,12 +70,22 @@ class ActionUmidade(Action):
         self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
 
+        response = verify_telegram(dispatcher, tracker)
+
+        if not response:
+            return []
+
+        dispatcher.utter_message(
+            text="Pegando a umidade para a fazenda de " + response['full_name'] + " ...")
+        time.sleep(2)
+
         value = random.randint(0, 100)
         text = "A umidade atual do olo é de {}%.".format(value)
 
         dispatcher.utter_message(text=text)
 
         return []
+
 
 class ActionPH(Action):
 
@@ -72,10 +96,18 @@ class ActionPH(Action):
         self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
 
+        response = verify_telegram(dispatcher, tracker)
+
+        if not response:
+            return []
+
+        dispatcher.utter_message(
+            text="Pegando o ph para a fazenda de " + response['full_name'] + " ...")
+        time.sleep(2)
+
         value = random.randint(1, 10)
         text = "O ph atual do solo é de {}.".format(value)
 
         dispatcher.utter_message(text=text)
 
         return []
-
